06ac00aa27d11e73c68f00535c55cb29e7fc1d42
Ramerson Rodrigues <ramersonrodrigues.ti@gmail.com>
2026-05-21 10:03:09 -0300

    atualização de dados

--- src/data/turmas.ts
+++ src/data/turmas.ts
@@ -77,6 +77,7 @@ const turmaB = new Turma(
     new Aluno("Gustavo", [
       new HistoricoItem("ganho", 10, "Exercicio", "07/03/2025", "POO"),
       new HistoricoItem("ganho", 10, "Apresentação trabalho", "15/03/2025", "IA"),
+      new HistoricoItem("ganho", 10, "Exercicio HTML e CSS", "15/03/2025", "Frontend"),
     ]),
      new Aluno("Alerrandro", [
       new HistoricoItem("ganho", 10, "Exercicio", "07/03/2025", "POO"),
